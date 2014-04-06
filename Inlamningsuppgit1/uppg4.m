962b84b8bbedfc4350ac3a231ba69c1bfa6906b0
Christofer Heimonen <christofer.heimonen@lunicore.se>
2014-04-06 22:19:03 +0200

    Task 1.4

--- Inlamningsuppgit1/uppg4.m
+++ Inlamningsuppgit1/uppg4.m
@@ -1,27 +1,38 @@
 function [ ] = uppg4( )
-%UPPG4 Summary of this function goes here
-%   Detailed explanation goes here
+%UPPG4 Plots a graph showing the mean computation time for Gaussian 
+% elimination and invert-by-multiply for systems with condition number
+%   10, 60, 110, 160,...,1010
+
 size = 10;
-i = 1;
 timesGauss = ones(20,1);
 timesInv = ones(20,1);
 conditionNumbers = ones(20,1);
+n = 10;
+timesGauss = ones(n, 20);
+timesInv = ones(n, 20);
+
 for i=1:20
-    conditionNumbers(i) = size;
-    A = makecond(size, 1);
-    x = rand([size,1]);
-    b = A*x;
-    tic;
-    x = A\b;
-    timeGauss = toc;
-    timesGauss(i) = timeGauss;
-    tic;
-    x2 = inv(A)*b;
-    timeInv = toc;
-    timesInv(i) = timeInv;
+    for j = 1:n
+        conditionNumbers(i) = size;
+        A = makecond(size, 1);
+        x = rand([size,1]);
+        b = A*x;
+        tic;
+        A\b;
+        timesGauss(j,i) = toc;
+        tic;
+        inv(A)*b;
+        timesInv(j,i) = toc;   
+    end
     size = size + 50;
 end
-size
-plot(conditionNumbers, timesGauss, conditionNumbers, timesInv)
+timeGauss = mean(timesGauss,1);
+timeInv = mean(timesInv,1);
+plot(conditionNumbers, timeGauss,'o', conditionNumbers, timeInv,'o')
+title('Compuation time for Gaussian elimination and invert-and-multiply for condition number')
+xlabel('Condition number')
+ylabel('Time (seconds)')
+hleg1= legend('time by Gaussian elimination','time by invert-and-multiply')
+set(hleg1,'Location','NorthWest')
 end
 
